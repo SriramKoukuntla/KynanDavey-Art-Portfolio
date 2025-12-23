e71d1b53a37a0ab5169ec71f7886fd434c20e96b
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-12-23 14:59:59 -0600

    Removed Carousel component and associated styles

--- src/sections/Photography.js
+++ src/sections/Photography.js
@@ -4,14 +4,14 @@ import Masonry from '../components/Masonry';
 import BirdsEating from '../assets/Art/PhotographicAndPhotoManipulationWorks/BirdsEating.png';
 import BirdsFlying from '../assets/Art/PhotographicAndPhotoManipulationWorks/BirdsFlying.png';
 import Branches from '../assets/Art/PhotographicAndPhotoManipulationWorks/Branches.png';
-import Eclipse from '../assets/Art/PhotographicAndPhotoManipulationWorks/Eclipse.png';
+// import Eclipse from '../assets/Art/PhotographicAndPhotoManipulationWorks/Eclipse.png';
 import Flame from '../assets/Art/PhotographicAndPhotoManipulationWorks/Flame.png';
 import Graphiti1 from '../assets/Art/PhotographicAndPhotoManipulationWorks/Graphiti1.png';
 import Graphiti2 from '../assets/Art/PhotographicAndPhotoManipulationWorks/Graphiti2.png';
 import Lights from '../assets/Art/PhotographicAndPhotoManipulationWorks/Lights.png';
 import POSTMORTEM from '../assets/Art/PhotographicAndPhotoManipulationWorks/POSTMORTEM.png';
 
-const items = [BirdsEating, BirdsFlying, Branches, Eclipse, Flame, Graphiti1, Graphiti2, Lights, POSTMORTEM];
+const items = [BirdsEating, BirdsFlying, Branches, Flame, Graphiti1, Graphiti2, Lights, POSTMORTEM];
 
 const Photography = () => {
   return (
